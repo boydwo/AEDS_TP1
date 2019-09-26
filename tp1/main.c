90c0c84ce30005cc87f81c52e656eb912aa84da0
vhvictorhugo <49657888+vhvictorhugo@users.noreply.github.com>
2019-09-25 23:52:17 -0300

    update do carai

--- tp1/main.c
+++ tp1/main.c
@@ -1,11 +1,34 @@
 #include "TAD_Matriz_Esparsa.h"
-
+#include <windows.h>
 int main(){
     TMatriz* Matriz;
-
-    leArquivo(&Matriz);
-
-    ImprimeMatriz (&Matriz);
-
+    int escolha;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+     while(escolha != 0){
+        printf("-----------------------------------------\n");
+        printf("-----------------MENU--------------------\n");
+        printf("1) Ler arquivo \n");
+        printf("2) Imprima a matriz \n");
+        printf("0) Sair \n");
+        printf("-----------------------------------------\n");
+        printf("-----------------------------------------\n");
+        switch (escolha)
+        {
+        case 1 :
+            leArquivo(&Matriz);
+            break;
+        case 2 :
+            SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+            ImprimeMatriz (&Matriz);
+            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+            break;
+        default:
+            SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+            printf("ERRO");
+            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+            break;
+        }
+    }
     return 0;
 }
